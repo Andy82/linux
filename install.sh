f1535223b8fe2275a03e6c7b81bf507e5dca3837
Andy82 <8501983+Andy82@users.noreply.github.com>
2025-10-05 13:22:38 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt install -y mc nano btop doublecmd-gtk  bleachbit conky-all freefilesync lollypop okular vlc remmina stacer syncthingtray tilix timeshift transmission-gtk hydra-gtk gnome-snapshot &&
+sudo snap install bitwarden && 
+sudo snap install obsidian --classic &&
+sudo snap install --classic code &&
+sudo snap install telegram-desktop &&
+sudo snap install chatgpt-desktop &&
+sudo add-apt-repository universe -y && sudo add-apt-repository ppa:agornostal/ulauncher-dev -y && sudo apt update && sudo apt install ulauncher -y &&
+sudo add-apt-repository ppa:unit193/encryption -y && sudo apt update && sudo apt install veracrypt -y
